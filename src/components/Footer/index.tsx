edd522e11b916d0fca3373c9c305eafe472fc69f
Lisette Svan <svanlisette@gmail.com>
2025-01-08 16:47:35 +0100

    making the components work together

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer className="bg-[#1D4E89] text-white py-12 mt-16 text-sm">
+        <footer className="text-white py-12 mt-16 text-sm">
             <div className="container mx-auto px-6 text-center rounded-xl max-w-90%">
-                <div className="mb-8">
+                <div className="mb-8 border-t-2 border-[#1D4E89]">
                     <a href="/" className="flex justify-center">
                         <img 
                             src="/images/logo.webp" 
@@ -14,26 +14,28 @@ const Footer = () => {
                     <p className="text-sm text-gray-300 mt-2">Your new favorite American all day brunch spot right in the heart of Stockholm!</p>
                 </div>
                 <div className="flex justify-center space-x-8 mb-8 text-xl">
-                    {/* <a href="/" className="text-sm text-gray-300 hover:text-[#F8F9FA]">Home</a>
-                    <a href="/menu" className="text-sm text-gray-300 hover:text-[#F8F9FA]">Menu</a>
-                    <a href="/about" className="text-sm text-gray-300 hover:text-[#F8F9FA]">About Us</a>
-                    <a href="/contact" className="text-sm text-gray-300 hover:text-[#F8F9FA]">Contact</a> */}
                     <a
                         href="https://www.foodora.se/restaurant/g24m/recos-chicken-and-waffles" 
                         className="order-link"
                     >
-                    Order!
-                </a>
-            </div>
-                <div className="flex justify-center space-x-6 mb-8">
-                    <a href="https://www.instagram.com/explore/locations/343401515524376/recos-chicken-and-waffles/" className="text-xl hover:text-white">
-                        <img src="/images/instag.png" alt="Instagram" width={24} height={24} />
-                    </a>
-                    <a href="https://www.tiktok.com/discover/reco-chicken-waffle-stockholm" className="text-xl">
-                        <img src="/images/tik-tok.png" alt="TikTok" width={24} height={24} />
+                        Order!
                     </a>
                 </div>
-                <div className="border-t border-gray-600 pt-6 pb-6">
+                <div className="flex justify-center space-x-12 mb-8">
+                    <div className="text-center">
+                        <p className="text-sm text-gray-300">Address: Reco's Chicken and Waffles, Bondegatan 13, 116 23 Stockholm, Sweden</p>
+                        <p className="text-sm text-gray-300">Phone: 08 640 02 71</p>
+                    </div>
+                </div>
+                <div className="border-t-2 border-[#1D4E89] pt-6 pb-6">
+                    <div className="flex justify-center space-x-6 mb-8">
+                        <a href="https://www.instagram.com/explore/locations/343401515524376/recos-chicken-and-waffles/" className="text-xl hover:text-white">
+                            <img src="/images/instag.png" alt="Instagram" width={24} height={24} />
+                        </a>
+                        <a href="https://www.tiktok.com/discover/reco-chicken-waffle-stockholm" className="text-xl">
+                            <img className="fill-white" src="/images/tik-tok.png" alt="TikTok" width={24} height={24} />
+                        </a>
+                    </div>
                     <p className="text-sm text-white font-extralight">© 2025 Reco's Chicken and Waffles. All Rights Reserved.</p>
                 </div>
             </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,68 @@
+'use client'
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
+const throttle = (func: { (): void; apply?: any; }, limit: number) => {
+    let lastFunc;
+    let lastRan;
+    return function () {
+        const context = this;
+        const args = arguments;
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(function () {
+                if (Date.now() - lastRan >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    };
+};
+
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = throttle(() => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        }, 100); 
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <div className="flex items-center justify-between bg-[#B22234] p-4 shadow-md">
-            <div className="flex justify-around flex-1 text-lg space-x-6">
+        <div className={`flex items-center justify-between bg-[#B22234] transition-all duration-200 ${isScrolled ? "p-2 pt-2 pb-2" : "p-2 pt-2 pb-2"} shadow-md border-b-2 border-[#1D4E89] sticky top-0 z-50`}>
+            <div className={`flex justify-around flex-1 text-lg space-x-6 ${isScrolled ? "text-lg" : ""}`}>
                 <Link href="/" className="text-[#F8F9FA] hover:text-[#1E3A8A] transition-colors ease-in-out duration-300">HOME</Link>
                 <Link href="/food" className="text-[#F8F9FA] hover:text-[#1E3A8A] transition-colors ease-in-out duration-300">FOOD</Link>
                 <Link href="/prices" className="text-[#F8F9FA] hover:text-[#1E3A8A] transition-colors ease-in-out duration-300">PRICES</Link>
             </div>
-            <div className="flex justify-center flex-shrink-0 transition-transform duration-300 transform hover:scale-110">
+
+            <div className={`flex justify-center flex-shrink-0 transition-transform duration-200 transform ${isScrolled ? "scale-75" : "scale-100"} will-change-transform`}>
                 <a href="/">
-                    <img 
-                        src="/images/logo2.png" 
-                        alt="Logo" 
+                    <img
+                        src="/images/logo2.png"
+                        alt="Logo"
                         className="object-contain"
-                        width={250}
+                        width={300} 
                         height={45}
                     />
                 </a>
             </div>
-            <div className="flex justify-around flex-1 text-lg space-x-6">
+
+            <div className={`flex justify-around flex-1 text-lg space-x-6 ${isScrolled ? "text-lg" : ""}`}>
                 <Link href="/order" className="text-[#F8F9FA] hover:text-[#1E3A8A] transition-colors ease-in-out duration-300">ORDER</Link>
                 <Link href="/contact" className="text-[#F8F9FA] hover:text-[#1E3A8A] transition-colors ease-in-out duration-300">CONTACT</Link>
                 <Link href="/about" className="text-[#F8F9FA] hover:text-[#1E3A8A] transition-colors ease-in-out duration-300">ABOUT</Link>
