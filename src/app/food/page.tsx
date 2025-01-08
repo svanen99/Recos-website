f38f7f0e2caa30170f82dc9c8ff5a3d481aa60cd
Lisette Svan <svanlisette@gmail.com>
2025-01-08 16:07:37 +0100

    viable informaiton in the page pages

---
+++ src/app/food/page.tsx
@@ -0,0 +1,8 @@
+export default function FoodPage() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to the Food page!</p>
+        </div>
+    );
+}
---
+++ src/app/prices/page.tsx
@@ -0,0 +1,8 @@
+export default function PricePage() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to the Price page!</p>
+        </div>
+    );
+}
