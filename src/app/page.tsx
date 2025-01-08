a8a068eb9a52deee404e105e47c3f95ec53f0f90
Lisette Svan <svanlisette@gmail.com>
2025-01-08 09:39:04 +0100

    header has all the functions for navigation now, maybe navigation will be deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Header from "@/components/Header";
 export default function Home() {
   return (
     <div>
-      <Navigation />
       <Header />
     </div>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,28 @@
+import Link from "next/link";
+
 const Header = () => {
     return (
-        <div className="flex justify-center cursor-pointer animate-pulse">
-            <a href="/">
-                <img 
-                    src="/images/logo2.png" 
-                    alt="Logo" 
-                    width={400} 
-                    height={150} 
-                />
-            </a>
+        <div className="flex items-center justify-between">
+            <div className="flex justify-around space-x-4 flex-1 text-xl">
+                <Link href="/" className="text-[#F8F9FA]">HOME</Link>
+                <Link href="/food" className="text-[#F8F9FA]">FOOD</Link>
+                <Link href="/prices" className="text-[#F8F9FA]">PRICES</Link>
+            </div>
+            <div className="flex justify-center flex-shrink-0">
+                <a href="/">
+                    <img 
+                        src="/images/logo2.png" 
+                        alt="Logo" 
+                        width={400} 
+                        height={150} 
+                    />
+                </a>
+            </div>
+            <div className="flex justify-around space-x-4 flex-1 text-xl">
+                <Link href="/order" className="text-[#F8F9FA]">ORDER</Link>
+                <Link href="/contact" className="text-[#F8F9FA]">CONTACT</Link>
+                <Link href="/about" className="text-[#F8F9FA]">ABOUT</Link>
+            </div>
         </div>
     );
 };
