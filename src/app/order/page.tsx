87e5931a2a5c44e18e905695a6f6d52b1d96bd3b
Lisette Svan <svanlisette@gmail.com>
2025-01-18 14:09:10 +0100

    fixed pages

--- src/app/order/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function OrderPage() {
-    return (
-        <div>
-            <h1>About Us</h1>
-            <p>Welcome to the Order page!</p>
-        </div>
-    );
-}
---
+++ src/app/update/page.tsx
@@ -0,0 +1,10 @@
+const Updatepage = () => {
+    return (
+        <div>
+            <h1>Update</h1>
+            <p>Welcome to the Update page!</p>
+        </div>
+    );
+}
+
+export default Updatepage;
