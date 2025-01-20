5a70c25b75fc7bde235f81c52bc25c203ae15530
Lisette Svan <svanlisette@gmail.com>
2025-01-20 13:40:30 +0100

    added more

--- src/app/update/page.tsx
+++ src/app/update/page.tsx
@@ -1,9 +1,10 @@
+import Update from "@/components/Update";
+
 const Updatepage = () => {
     return (
-        <div>
-            <h1>Update</h1>
-            <p>Welcome to the Update page!</p>
-        </div>
+       <>
+        <Update />
+       </>
     );
 }
 
